9e41898242a299f03e371e167feb5e9a886d9e2a
Deluxe Editor HOBBY GITHUB Profile <reich2824@gmail.com>
2025-05-16 18:16:32 +0700

    [refactor] Implement singleton pattern for Window class and update main.py to create multiple instances

--- daw/main.py
+++ daw/main.py
@@ -2,7 +2,13 @@ from mainwindow import Window
 
 if __name__ == "__main__":
     handle = Window()
+    handle2 = Window()
     handle.title("DAW")
     handle.geometry("1280x720")
+    handle2.title("DAW")
+    handle2.geometry("1280x720")
+
+    assert(handle is handle2) # Without singleton pattern, this should fail that the two windows are not the same object (instance)
 
     handle.mainloop()
+    handle2.mainloop()
